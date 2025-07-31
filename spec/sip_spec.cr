75bcdf256503d3445b20d36fe165f9bd36843c40
Jovany Leandro G.C <bit4bit@riseup.net>
2025-07-31 19:14:15 +0300

    Feat SIP parser

---
+++ spec/sip_spec.cr
@@ -0,0 +1,17 @@
+require "./spec_helper"
+
+describe Agent::Network::SIP do
+  it "parse SIP Response" do
+    response = Agent::Network::SIP(Agent::Network::SIP::Response).parse(IO::Memory.new("SIP/2.0 200 OK\r\nVia: SIP/2.0/UDP 192.168.1.1:5060;branch=z9hG4bK776asdhj\r\nFrom: Alice <sip:alice@example.com>;tag=12345\r\nTo: Bob <sip:bob@example.com>;tag=67890\r\nCall-ID: 1234567890@example.com\r\nCSeq: 1 INVITE\r\nContent-Length: 0\r\n\r\n"))
+    response.status_code.should eq 200
+    response.headers["Via"].should eq("SIP/2.0/UDP 192.168.1.1:5060;branch=z9hG4bK776asdhj")
+  end
+
+  it "parse SIP Request" do
+    request = Agent::Network::SIP(Agent::Network::SIP::Request).parse(IO::Memory.new("INVITE sip:bob@example.com SIP/2.0\r\nVia: SIP/2.0/UDP 192.168.1.1:5060;branch=z9hG4bK776asdhj\r\nFrom: Alice <sip:alice@example.com>;tag=12345\r\nTo: Bob <sip:bob@example.com>;tag=67890\r\nCall-ID: 1234567890@example.com\r\nCSeq: 1 INVITE\r\nContent-Length: 0\r\n\r\n"))
+    request.method.should eq "INVITE"
+    request.uri.should eq "sip:bob@example.com"
+    request.version.should eq "SIP/2.0"
+    request.headers["To"].should eq("Bob <sip:bob@example.com>;tag=67890")
+  end
+end
